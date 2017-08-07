31f5602e6bb9fdc2bcaf96b7c1967e166cb7b7d4
Jay Baker <logikal@gmail.com>
2017-08-08 09:39:33 +1200

    test(Helpers): adding tests for helpers

---
+++ test/index.js
@@ -0,0 +1,18 @@
+const { expect } = require('code')
+const { describe, before, it } = require('mocha')
+const Helpers = require('../lib')
+const { name, version } = require('../package.json')
+
+describe('Create bundle', () => {
+  it(`should return the correct package name`, done => {
+    var bundleName = Helpers.getBundleName()
+    expect(bundleName).to.equal(`${name}.${version}.zip`)
+    done()
+  })
+
+  it(`should return a string of files to bundle`, done => {
+    var files = Helpers.getBundleFiles()
+    expect(files).to.be.string()
+    done()
+  })
+})
